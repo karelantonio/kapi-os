8e883a5778484697ce164679374a331bdf86f058
Karel <karelantoniogz@gmail.com>
2024-09-03 12:38:59 -0400

    Added pointer utilities

---
+++ include/ptr_utils.h
@@ -0,0 +1,9 @@
+#ifndef PTR_UTILS_H
+#define PTR_UTILS_H
+
+// Some utilities when working on pointers
+
+#define NULL ((void*)0)
+#define PROPAGATE_NULL(val) if( (val)==NULL ) return NULL; else {};
+
+#endif
